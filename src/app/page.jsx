7fac4482128fd8b4535435379f0336370a15a27b
aminurrahman4078 <aminurrahman9793@gmail.com>
2026-02-16 21:27:59 +0600

    added some home component

---
+++ src/app/page.jsx
@@ -0,0 +1,42 @@
+import Hero from "@/component/Home/Hero";
+import CategoryRibbon from "@/component/Home/CategoryRibbon";
+import FlashSale from "@/component/Home/FlashSale";
+import BentoGrid from "@/component/Home/BentoGrid";
+import NewArrivalsScroll from "@/component/Home/NewArrivalsScroll";
+import BrandLogos from "@/component/Home/BrandLogos";
+import Testimonial from "@/component/Home/Testimonial";
+import TrustSection from "@/component/Home/TrustSection"; // Fixed import
+import Newsletter from "@/component/Home/Newsletter";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      {/* 1. Hero / Main Visual */}
+      <Hero />
+
+      {/* 2. Quick Navigation */}
+      <CategoryRibbon />
+
+      {/* 3. Urgency / Conversion */}
+      <FlashSale />
+
+      {/* 4. Curated Collections */}
+      <BentoGrid />
+
+      {/* 5. Fresh Inventory */}
+      <NewArrivalsScroll />
+
+      {/* 6. Social Proof */}
+      <Testimonial />
+
+      {/* 7. Marketplace Authority */}
+      <BrandLogos />
+
+      {/* 8. Safety & Trust */}
+      <TrustSection />
+
+      {/* 9. Lead Generation (Best placed at the end) */}
+      <Newsletter />
+    </main>
+  );
+}
